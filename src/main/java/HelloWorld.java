a0dc61aaca436c3ac1719bccc53bf1e867007d17
drvdevops2805 <75531566+drvdevops2805@users.noreply.github.com>
2021-01-05 13:05:12 +0530

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -9,6 +9,6 @@ public class HelloWorld implements SparkApplication {
 
 	@Override
 	public void init() {
-		get("/hello1", (req, res) -> "Hello Dhruv! This is your first pipeline deployment..Enjoy!");
+		get("/hello1", (req, res) -> "Hello DRV! This is your first pipeline deployment..Enjoy!");
 	}
 }
